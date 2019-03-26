e515032be96a6034b98cf77879640e9ac703c056
JamesHujy <48405323+JamesHujy@users.noreply.github.com>
2019-03-27 00:46:22 +0800

    final_version but_no_frequency

--- Bayes.py
+++ Bayes.py
@@ -10,8 +10,8 @@ class Dataset(object):
 	"""
 	build a reader class for readthe data
 	"""
-	def __init__(self, filename):
-		print("processing the words...")
+	def __init__(self, filename, add_feature):
+		print("Loading the emails...")
 		self.filename = filename
 		self.emaillist = []
 		self.labels = []
@@ -19,6 +19,7 @@ class Dataset(object):
 		self.hamtimes = 0
 		self.Fromlist = []
 		self.timelist = []
+		self.add_feature = add_feature
 
 	def generateindex(self, index):
 		if index < 10:
@@ -29,24 +30,18 @@ class Dataset(object):
 			return str(index)
 
 	def getword(self, index1, index2):
-		path = self.filename+'/data_cut/'+index1+"/"+index2
+		path = "./"+self.filename+'/data_cut/'+index1+"/"+index2
 		f = codecs.open(path,encoding='utf-8')
 		string = f.read()
-		string = re.sub(r"[\s：、。，,.;：；_★:()-?!@#^&$¥……（）◆\]\[]", " ", string)
+		if self.add_feature:
+			string = re.sub(r"[\s：、。，,.;：；_:()-?!@#^&……（）\]\[]", " ", string)
+		else:
+			string = re.sub(r"[\s：、。，,.;：；_★:()-?!@#^&$¥……（）◆\]\[]", " ", string)
 		string = re.sub(r"[a-zA-Z]"," ",string)
 		return string
 
-	def gettime(self, index1, index2):
-		path = self.filename+'/data_cut/'+index1+"/"+index2
-		f = codecs.open(path,encoding='utf-8')
-		string = f.readlines()
-		timestring = string[2]
-		timestring = re.findall("\d\d:\d\d:\d\d",timestring)
-		return timestring
-
 	def getFrom(self, index1, index2):
-		
-		path = self.filename+'/data_cut/'+index1+"/"+index2
+		path = "./"+self.filename+'/data_cut/'+index1+"/"+index2
 		f = codecs.open(path,encoding='utf-8')
 		string = f.readlines()
 		i = 0
@@ -56,9 +51,7 @@ class Dataset(object):
 				return []
 		Fromstring = string[i]
 		Fromstring = re.findall("@\w*",Fromstring)
-		finalFromstring = Fromstring[1:]
-		print(finalFromstring)
-		return finalFromstring
+		return Fromstring
 
 	def readlist(self):
 		for i in tqdm.tqdm(range(215)):
@@ -67,6 +60,8 @@ class Dataset(object):
 				string = self.getword(self.generateindex(i),self.generateindex(j))
 				content = string.split()
 				content = [con for con in content if len(con) >= 2]
+				if self.add_feature:
+					content.extend(self.getFrom(self.generateindex(i),self.generateindex(j)))
 				content = list(set(content))
 				self.emaillist.append(content)
 				j += 1
@@ -74,35 +69,11 @@ class Dataset(object):
 			string = self.getword("215",self.generateindex(i))
 			content = string.split()
 			content = [con for con in content if len(con) >= 2]
+			if self.add_feature:
+					content.extend(self.getFrom(self.generateindex(215),self.generateindex(i)))
 			content = list(set(content))
 			self.emaillist.append(content)
 		return self.emaillist
-
-	def gettimelist(self):
-		for i in tqdm.tqdm(range(215)):
-			j = 0
-			while j < 300:
-				timestring = self.gettime(self.generateindex(i),self.generateindex(j))
-				self.timelist.append(timestring)
-				j += 1
-		for i in range(120):
-			timestring = self.gettime("215",self.generateindex(i))
-			self.timelist.append(timestring)
-		return self.timelist
-
-	def getFromlist(self):
-		print("Loading the Receiver email address")
-		for i in tqdm.tqdm(range(215)):
-			j = 0
-			while j < 300:
-				print(i," ",j)
-				Fromstring = self.getFrom(self.generateindex(i),self.generateindex(j))
-				self.Fromlist.append(Fromstring)
-				j += 1
-		for i in range(120):
-			Fromtring = self.getFrom("215",self.generateindex(i))
-			self.Fromlist.append(Fromstring)
-		return self.Fromlist
 
 	def getlabels(self):
 		path = self.filename+"/label/index"
@@ -126,9 +97,6 @@ class Trainer(object):
 		self.hamdict = {}
 		self.emaillist = Dataset.readlist()
 		self.labels = Dataset.getlabels()
-		self.timelist = Dataset.gettimelist()
-		self.Fromlist = Dataset.getFromlist()
-		#print(self.timelist)
 		self.hamtimes = 0
 		self.spamtimes = 0
 		self.total = 215 * 300 + 119
@@ -165,7 +133,10 @@ class Trainer(object):
 						self.hamdict[word] = 1
 
 	def testone(self):
-		correct_times = 0
+		spam_correct_times = 0
+		ham_correct_times = 0
+		spamtotal = 0
+		hamtotal = 0
 		testnum = self.total * self.trainpercent/ 500
 		for i in self.testindex:
 			logspam = 0.0
@@ -173,9 +144,7 @@ class Trainer(object):
 			for word in self.emaillist[i]:
 				if word in self.spamdict.keys():
 					logspam += math.log(self.spamdict[word] / self.spamtimes)
-					#logspam += math.log(self.spamdict[word] / self.spamtimes)
 				else:
-					#logspam += math.log(1 / (self.spamtimes + 2))
 					logspam += math.log(1 / (self.spamtimes + 2))
 				if word in self.hamdict.keys():
 					logham += math.log(self.hamdict[word] / self.hamtimes)
@@ -188,33 +157,45 @@ class Trainer(object):
 				judge = True
 			else:
 				judge = False
-			if judge == True and self.labels[i] == '0':
-				correct_times += 1
-			elif judge == False and self.labels[i] == '1':
-				correct_times += 1
+			if self.labels[i] == '0':
+				spamtotal += 1
 			else:
-				pass
-				#print(i)
-				#print(self.emaillist[i])
-		self.spamtimes = 0
-		self.hamtimes = 0
+				hamtotal += 1
+			if judge == True and self.labels[i] == '0':
+				spam_correct_times += 1
+			if judge == False and self.labels[i] == '1':
+				ham_correct_times += 1
 		self.hamdict.clear()
 		self.spamdict.clear()
+		correct_times = spam_correct_times+ham_correct_times
 		accuracy = correct_times / testnum
-		return accuracy
+		precision = ham_correct_times / hamtotal
+		recall = spam_correct_times / spamtotal
+		F1_score = 2 * precision * recall/(precision+recall)
+		self.spamtimes = 0
+		self.hamtimes = 0
+		return accuracy, precision, recall, F1_score
 
 	def test(self):
 		accuracy = 0
+		precision = 0
+		recall = 0
+		F1 = 0
 		accuracylist = []
-		for i in range(5):
+		print("Training the model...")
+		for i in tqdm.tqdm(range(5)):
 			self.cut((i+1))
 			self.train()
-			print(self.hamtimes," ",self.spamtimes)
-			oneaccuracy = self.testone()
+			oneaccuracy,oneprecision,onerecall,oneF1 = self.testone()
 			accuracy += oneaccuracy
+			precision += oneprecision
+			recall += onerecall
+			F1 += oneF1
 			accuracylist.append(oneaccuracy)
-		print(accuracylist)	
-		print(accuracy/5)
+		print("accuracy: ",accuracy/5)
+		print("precision: ",precision/5)
+		print("recall: ",recall/5)
+		print("F1: ",F1/5)
 
 def main():
 	import argparse
@@ -222,18 +203,13 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-n","--filename",type=str,default="trec06c-utf8")
 	parser.add_argument("-p","--percent",type=int,default=100)
-	parser.add_argument("-r","--randomseed",type=int,default=100)
+	parser.add_argument("-r","--randomseed",type=int,default=233)
+	parser.add_argument("-d","--add_feature",type=bool,default=False)
 	args = parser.parse_args()
 
-	data = Dataset(args.filename)
+	data = Dataset(args.filename,args.add_feature)
 	trainer = Trainer(data)
 	trainer.decidetrain(args.randomseed,args.percent)
 	trainer.test()
 
-main()
-
-
-
-
-
-	
+main()	
